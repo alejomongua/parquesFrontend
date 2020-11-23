c68b5e70f4a1d8b0b350e9e7722b598e8371aa6a
Alejandro Mongua <alejandro.mongua@gmail.com>
2020-11-22 20:57:26 -0500

    Se puede listar exitosamente los juegos públicos

--- src/Pages/Listar.vue
+++ src/Pages/Listar.vue
@@ -1,46 +1,84 @@
 <template>
   <div class='container w-75 m-auto'>
-    <h1 class='w-full flex text-center text-3xl leading-9 font-extrabold
-               mt-6 text-gray-700'>
+    <h1 class='w-full text-center text-3xl leading-9 font-extrabold
+               my-6 text-gray-700'>
       Listar partidas públicas
     </h1>
-    <div class="w-full flex">
-      <table class="table-fixed w-full">
-        <body>
+    <div class="w-full mt-2">
+      <table class="table-auto m-auto">
+        <thead>
+          <tr>
+            <th class='bg-blue-100 border text-left px-8 py-4'>ID</th>
+            <th class='bg-blue-100 border text-left px-8 py-4'>Inscr/Posic</th>
+            <th class='bg-blue-100 border text-left px-8 py-4'>Creado</th>
+            <th class='bg-blue-100 border text-left px-8 py-4'>Acción</th>
+          </tr>
+        </thead>
+        <tbody>
           <tr
-            :key='juego.id'
-            v-for='juego in juegos'
+            class='w-full'
+            :key='juego[0]'
+            v-for='juego in juegosOrdenados'
           >
-            <td class="whitespace-no-wrap">
-              {{ juego.jugadores }}/{{ juego.posiciones }}
+            <td class="whitespace-no-wrap border px-8 py-2">
+              {{ juego[0] }}
             </td>
-            <td class="whitespace-no-wrap">
-              {{ timeago(juego.created_at * 1000) }}
+            <td class="whitespace-no-wrap border px-8 py-2">
+              {{ juegos[juego[0]].jugadores }}/{{ juegos[juego[0]].posiciones }}
             </td>
-            <td class="text-center">
-              <button @click="unirse(juego.id)">
+            <td class="whitespace-no-wrap border px-8 py-2">
+              {{ timeago(juego[1] * 1000) }}
+            </td>
+            <td class="text-center border px-8 py-2">
+              <button
+                class='bg-blue-500 hover:bg-blue-700 text-white font-bold p-1 m-2 rounded'
+                @click="unirse(juego[0])">
                 Unirse
               </button>
             </td>
           </tr>
-        </body>
+        </tbody>
       </table>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { timeago } from '../dateHelpers'
-
 import { defineComponent } from 'vue'
+
+import { timeago } from '../dateHelpers'
+import api, { APIError, ListadoJuegosPublicos } from '../api'
+
+// type guard functions
+function isAPIError (response: APIError | ListadoJuegosPublicos): response is APIError {
+  return (typeof response.mensaje === 'string')
+}
+
 export default defineComponent({
   data () {
+    const juegos:ListadoJuegosPublicos = {}
+    const juegosOrdenados:[string, number][] = []
     return {
-      juegos: [],
+      juegos,
+      juegosOrdenados
     }
   },
-  beforeCreate () {
+  mounted: async function () {
+    const juegos = await api.juegosPublicos()
+    if (isAPIError(juegos)) {
+      console.error(juegos.mensaje)
+      // To do: mostrar error al usuario
+      return
+    }
 
+    this.juegos = juegos
+    const juegosOrdenados:[string, number][] = []
+    Object.keys(juegos).forEach((juegoId:string) => {
+      const juego = juegos[juegoId]
+      juegosOrdenados.push([juegoId, juego.created_at])
+    })
+
+    this.juegosOrdenados = juegosOrdenados.sort((elemento1, elemento2) => elemento2[1] - elemento1[1])
   },
   methods: {
     timeago,
--- src/dateHelpers.ts
+++ src/dateHelpers.ts
@@ -1,38 +1,38 @@
-export const timeago = (ms:number) => {
-  let ago = Math.floor(ms / 1000)
+export const timeago = (seconds:number) => {
+  let ago = ((new Date()).getTime() - seconds) / 1000
   let part = 0
 
   if (ago < 5) { return 'justo ahora' }
-  if (ago < 60) { return ago + 'hace unos segundos' }
+  if (ago < 60) { return `hace ${ago} unos segundos` }
 
   if (ago < 120) { return 'hace un minuto' }
   if (ago < 3600) {
     while (ago >= 60) { ago -= 60; part += 1 }
-    return part + 'hace unos minutos'
+    return `hace ${part} minutos`
   }
 
   if (ago < 7200) { return 'hace una hora' }
   if (ago < 86400) {
     while (ago >= 3600) { ago -= 3600; part += 1 }
-    return part + 'hace unas horas'
+    return `hace ${part} horas`
   }
 
   if (ago < 172800) { return 'hace un dia' }
   if (ago < 604800) {
     while (ago >= 172800) { ago -= 172800; part += 1 }
-    return part + 'hace unos dias'
+    return `hace ${part} dias`
   }
 
   if (ago < 1209600) { return 'hace una semana' }
   if (ago < 2592000) {
     while (ago >= 604800) { ago -= 604800; part += 1 }
-    return part + 'hace unas semanas'
+    return `hace ${part} semanas`
   }
 
   if (ago < 5184000) { return 'hace un mes' }
   if (ago < 31536000) {
     while (ago >= 2592000) { ago -= 2592000; part += 1 }
-    return part + 'hace unos meses'
+    return `hace ${part} meses`
   }
 
   if (ago < 1419120000) { // 45 years, approximately the epoch
@@ -40,5 +40,5 @@ export const timeago = (ms:number) => {
   }
 
   // TODO pass in Date.now() and ms to check for 0 as never
-  return 'hace muchos, muchos años'
+  return 'hace mucho tiempo'
 }
